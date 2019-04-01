85e9ad17b4f14e1a3dcd322850a2862db023a65b
wipegup <wipegup@gmail.com>
2019-04-01 17:03:45 -0600

    Add tests for user roles

---
+++ spec/models/user_roles_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe User do
+  describe 'roles' do
+    it 'can create an admin' do
+      admin = create(:admin)
+
+      expect(user.role).to eq("admin")
+      expect(user.admin?).to be_truthy
+    end
+
+    it 'can create a merchang' do
+      admin = create(:merchant)
+
+      expect(user.role).to eq("merchant")
+      expect(user.merchant?).to be_truthy
+    end
+    it 'can create an admin' do
+      admin = create(:customer)
+
+      expect(user.role).to eq("customer")
+      expect(user.customer?).to be_truthy
+    end
+  end
+end
