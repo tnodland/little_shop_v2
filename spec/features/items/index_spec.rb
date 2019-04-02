60d4c369181d01cd4c550d9ddcfa627b65f1b132
Trevor Nodland <trevor.nodland@gmail.com>
2019-04-02 16:19:56 -0600

    adds test for user story 15

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -32,5 +32,49 @@ RSpec.describe "Items index page" do
 
       expect(page).to_not have_content(@item3.name)
     end
+
+    it "can see statistics about popular items" do
+      item4 = create(:item, user: @merchant1)
+      item5 = create(:item, user: @merchant1)
+      item6 = create(:item, user: @merchant1)
+      item7 = create(:item, user: @merchant1)
+      item8 = create(:item, user: @merchant1)
+      item9 = create(:item, user: @merchant1)
+      item10 = create(:item, user: @merchant1)
+
+      shopper = create(:user)
+      order = create(:shipped_order, user: shopper)
+
+      create(:fulfilled_order_item, order: order, item: @item1, quantity: @item1.quantity)
+      create(:fulfilled_order_item, order: order, item: @item2, quantity: @item2.quantity)
+      create(:fulfilled_order_item, order: order, item: @item3, quantity: @item3.quantity)
+      create(:fulfilled_order_item, order: order, item: item4, quantity: item4.quantity)
+      create(:fulfilled_order_item, order: order, item: item5, quantity: item5.quantity)
+      create(:fulfilled_order_item, order: order, item: item6, quantity: item6.quantity)
+      create(:fulfilled_order_item, order: order, item: item7, quantity: item7.quantity)
+      create(:fulfilled_order_item, order: order, item: item8, quantity: item8.quantity)
+      create(:fulfilled_order_item, order: order, item: item9, quantity: item9.quantity)
+      create(:fulfilled_order_item, order: order, item: item10, quantity: item10.quantity)
+
+      visit items_path
+
+      within "most-popular-items" do
+        expect(page).to have_content("5 Most Popular Items:")
+        expect(page.all("#item")[0]).to have_content("#{item10.name}: #{item10.total_sold} sold")
+        expect(page.all("#item")[1]).to have_content("#{item9.name}: #{item9.total_sold} sold")
+        expect(page.all("#item")[2]).to have_content("#{item8.name}: #{item8.total_sold} sold")
+        expect(page.all("#item")[3]).to have_content("#{item7.name}: #{item7.total_sold} sold")
+        expect(page.all("#item")[4]).to have_content("#{item6.name}: #{item6.total_sold} sold")
+      end
+
+      within "least-popular-items" do
+        expect(page).to have_content("5 Least Popular Items:")
+        expect(page.all("#item")[0]).to have_content("#{item1.name}: #{item1.total_sold} sold")
+        expect(page.all("#item")[1]).to have_content("#{item2.name}: #{item2.total_sold} sold")
+        expect(page.all("#item")[2]).to have_content("#{item3.name}: #{item3.total_sold} sold")
+        expect(page.all("#item")[3]).to have_content("#{item4.name}: #{item4.total_sold} sold")
+        expect(page.all("#item")[4]).to have_content("#{item5.name}: #{item5.total_sold} sold")
+      end
+    end
   end
 end
