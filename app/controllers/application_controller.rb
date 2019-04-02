e08d47f32d00e210ca97f27646759fb96121e3d3
wipegup <wipegup@gmail.com>
2019-04-02 11:43:23 -0600

    Add customer / cart controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def current_customer?
+    current_user == nil || current_user&.user?
+  end
 end
