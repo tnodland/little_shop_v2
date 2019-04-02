6e051a2382c41e64856acf217a0c39a3f37523e1
wipegup <wipegup@gmail.com>
2019-04-02 09:50:34 -0600

    Change 'customer' to 'user'

--- spec/features/user_authorized_navigation_spec.rb
+++ spec/features/user_authorized_navigation_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'User' do
   before :each do
-    @customer = create(:customer)
+    @user = create(:user)
     @admin = create(:admin)
     @merchant = create(:merchant)
     @error_content = "The page you were looking for doesn't exist"
@@ -16,9 +16,9 @@ RSpec.describe 'User' do
       end
     end
 
-    context 'as a customer' do
+    context 'as a user' do
       it 'shows the profile' do
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@customer)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
         visit profile_path
         expect(page).not_to have_content(@error_content)
       end
@@ -49,9 +49,9 @@ RSpec.describe 'User' do
       end
     end
 
-    context 'as a customer' do
+    context 'as a user' do
       it 'gives 404' do
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@customer)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
         visit dashboard_path
         expect(page).to have_content(@error_content)
       end
@@ -82,9 +82,9 @@ RSpec.describe 'User' do
       end
     end
 
-    context 'as a customer' do
+    context 'as a user' do
       it 'shows the cart' do
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@customer)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
         visit cart_path
         expect(page).not_to have_content(@error_content)
       end
@@ -124,9 +124,9 @@ RSpec.describe 'User' do
       end
     end
 
-    context 'as a customer' do
+    context 'as a user' do
       it 'gives 404' do
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@customer)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
         visit admin_dashboard_path
         expect(page).to have_content(@error_content)
 
