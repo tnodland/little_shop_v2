c084d0c8f303bc1a1300419bb19234d370d159a1
wipegup <wipegup@gmail.com>
2019-04-02 11:32:55 -0600

    Added all routes for auth tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    get '/dashboard', to: "orders#index"
+  end
+
+  get '/dashboard', to: "items#index"
+  get '/profile', to: "users#show"
+  get '/cart', to: "cart#show"
 end
