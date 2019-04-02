7af545c211357eaa7f3dd7ce6922a02e4ff93bed
Trevor Nodland <trevor.nodland@gmail.com>
2019-04-02 11:00:33 -0600

    updates models with validates_inclusion_of to validate boolean attributes
    Co-authored-by: Jon Peterson <3322920+joequincy@users.noreply.github.com>
    Co-authored-by: William Peterson <29714514+wipegup@users.noreply.github.com>

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,4 +6,6 @@ class OrderItem < ApplicationRecord
                         :item_id,
                         :quantity,
                         :ordered_price
+
+  validates_inclusion_of :fulfilled, in: [true, false]
 end
