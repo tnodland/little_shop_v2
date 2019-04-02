0b5e7897718688764e3ae97c50c044802d45e516
wipegup <wipegup@gmail.com>
2019-04-02 14:53:02 -0600

    Add route for admin user index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get '/dashboard', to: "orders#index"
+    resources :users, only: [:index]
   end
 
   get '/dashboard', to: "merchant/items#index"
