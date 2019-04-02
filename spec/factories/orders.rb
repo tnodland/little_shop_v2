8b59b6149e4c5e1a3265e70be415b01294045b6b
Trevor Nodland <trevor.nodland@gmail.com>
2019-04-02 11:10:44 -0600

    updates orders factory to include packaged, shipped and cancelled orders

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :order do
     user
+
+    status { 0 }
+  end
+
+  factory :packaged_order, parent: :order do
+    status { 1 }
+  end
+
+  factory :shipped_order, parent: :order do
+    status { 2 }
+  end
+
+  factory :cancelled_order, parent: :order do
+    status { 3 }
   end
 end
