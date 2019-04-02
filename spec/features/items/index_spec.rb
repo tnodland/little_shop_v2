1f41a085509cba09f2dc942f4e64706b773bcac1
Trevor Nodland <trevor.nodland@gmail.com>
2019-04-02 11:47:11 -0600

    finishes user story 14

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -10,18 +10,18 @@ RSpec.describe "Items index page" do
       item3 = create(:inactive_item, user: merchant1)
 
       visit items_path
-
+      
       within "#item-#{item1.id}" do
         expect(page).to have_link(item1.name)
-        expect(page).to have_content(item1.image_url)
-        expect(page).to have_content(merchant1.name)
-        expect(page).to have_content(item1.quantity)
-        expect(page).to have_content(item1.current_price)
+        expect(page).to have_xpath("//img[@src='https://vignette.wikia.nocookie.net/animalcrossing/images/7/72/Tom_Nook.png/revision/latest?cb=20101105231130']")
+        expect(page).to have_content("Sold by: #{merchant1.name}")
+        expect(page).to have_content("Quantity: #{item1.quantity}")
+        expect(page).to have_content("Price: $#{item1.current_price}")
       end
 
       within "#item-#{item2.id}" do
         expect(page).to have_link(item2.name)
-        expect(page).to have_content(item2.image_url)
+        expect(page).to have_xpath("//img[@src='https://vignette.wikia.nocookie.net/animalcrossing/images/7/72/Tom_Nook.png/revision/latest?cb=20101105231130']")
         expect(page).to have_content(merchant2.name)
         expect(page).to have_content(item2.quantity)
         expect(page).to have_content(item2.current_price)
