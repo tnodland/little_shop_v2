9574e3cbd53a05bcf34fe85c4ae1c76b75cc4aea
wipegup <wipegup@gmail.com>
2019-04-02 14:56:22 -0600

    Passing admin user show test

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get '/dashboard', to: "orders#index"
-    resources :users, only: [:index]
+    resources :users, only: [:index, :show]
   end
 
   get '/dashboard', to: "merchant/items#index"
