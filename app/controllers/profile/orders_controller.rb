a273713613149873f798998ebcb2113afe902552
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-04-03 15:05:22 -0600

    Implements order cancellation functionality and hides button when order is not pending

--- app/controllers/profile/orders_controller.rb
+++ app/controllers/profile/orders_controller.rb
@@ -8,4 +8,24 @@ class Profile::OrdersController < Profile::BaseController
     @order = Order.find(params[:id])
     @line_items = @order.order_items
   end
+
+  def cancel
+    order = Order.find(params[:id])
+    if order.pending?
+      order.order_items.each do |order_item|
+        if order_item.fulfilled?
+          order_item.fulfilled = false
+          order_item.item.quantity += order_item.quantity
+          order_item.item.save
+          order_item.save
+        end
+      end
+      order.status = 3
+      order.save
+      flash[:notice] = "Your order has been cancelled"
+      redirect_to profile_path
+    else
+      redirect_to profile_order_path(params[:id])
+    end
+  end
 end
