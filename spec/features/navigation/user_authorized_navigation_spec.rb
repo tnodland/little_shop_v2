3eecfe333cdb5b394a6d618fcb9d7978194298d5
wipegup <wipegup@gmail.com>
2019-04-03 16:03:24 -0600

    Reorganize authorized navigation tests

--- spec/features/navigation/user_authorized_navigation_spec.rb
+++ spec/features/navigation/user_authorized_navigation_spec.rb
@@ -1,178 +1,158 @@
 require 'rails_helper'
 
-RSpec.describe 'User' do
+RSpec.describe 'as a visitor' do
   before :each do
-    @user = create(:user)
-    @admin = create(:admin)
-    @merchant = create(:merchant)
     @error_content = "The page you were looking for doesn't exist"
+    user = create(:user)
   end
 
-  describe 'visits /profile' do
-    context 'as a visitor' do
-      it 'gives 404' do
-        visit profile_path
-        expect(page).to have_content(@error_content)
-      end
-    end
-
-    context 'as a user' do
-      it 'shows the profile' do
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-        visit profile_path
-        expect(page).not_to have_content(@error_content)
-      end
-    end
-
-    context 'as a merchant' do
-      it 'gives 404' do
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
-        visit profile_path
-        expect(page).to have_content(@error_content)
-      end
-    end
-
-    context 'as an admin' do
-      it 'give 404' do
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
-        visit profile_path
-        expect(page).to have_content(@error_content)
-      end
-    end
+  it 'gives 404 at profile page' do
+    visit profile_path
+    expect(page).to have_content(@error_content)
   end
 
-  describe 'visits /dashboard' do
-    context 'as a visitor' do
-      it 'gives 404' do
-        visit dashboard_path
-        expect(page).to have_content(@error_content)
-      end
-    end
-
-    context 'as a user' do
-      it 'gives 404' do
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-        visit dashboard_path
-        expect(page).to have_content(@error_content)
-      end
-    end
-
-    context 'as a merchant' do
-      it 'shows the dashboard' do
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
-        visit dashboard_path
-        expect(page).not_to have_content(@error_content)
-      end
-    end
-
-    context 'as an admin' do
-      it 'give 404' do
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
-        visit dashboard_path
-        expect(page).to have_content(@error_content)
-      end
-    end
+  it 'gives 404 at dashboard' do
+    visit dashboard_path
+    expect(page).to have_content(@error_content)
   end
 
-  describe 'visits /cart' do
-    context 'as a visitor' do
-      it 'shows the cart' do
-        visit cart_path
-        expect(page).not_to have_content(@error_content)
-      end
-    end
-
-    context 'as a user' do
-      it 'shows the cart' do
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-        visit cart_path
-        expect(page).not_to have_content(@error_content)
-      end
-    end
-
-    context 'as a merchant' do
-      it 'gives 404' do
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
-        visit cart_path
-        expect(page).to have_content(@error_content)
-      end
-    end
-
-    context 'as an admin' do
-      it 'give 404' do
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
-        visit cart_path
-        expect(page).to have_content(@error_content)
-      end
-    end
+  it 'shows the cart' do
+    visit cart_path
+    expect(page).not_to have_content(@error_content)
   end
 
-  describe 'visits any /admin path' do
-    context 'as a visitor' do
-      it 'gives 404' do
-        visit admin_dashboard_path
-        expect(page).to have_content(@error_content)
+  context 'visit any admin path' do
+    it 'gives 404' do
+      visit admin_dashboard_path
+      expect(page).to have_content(@error_content)
 
-        visit admin_users_path
-        expect(page).to have_content(@error_content)
+      visit admin_users_path
+      expect(page).to have_content(@error_content)
 
-        visit admin_user_path(User.last)
-        expect(page).to have_content(@error_content)
+      visit admin_user_path(User.last)
+      expect(page).to have_content(@error_content)
 
-        visit admin_merchant_path(User.last)
-        expect(page).to have_content(@error_content)
-      end
+      visit admin_merchant_path(User.last)
+      expect(page).to have_content(@error_content)
     end
+  end
+end
 
-    context 'as a user' do
-      it 'gives 404' do
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-        visit admin_dashboard_path
-        expect(page).to have_content(@error_content)
+RSpec.describe 'as a user' do
+  before :each do
+    @error_content = "The page you were looking for doesn't exist"
+    @user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  end
 
-        visit admin_users_path
-        expect(page).to have_content(@error_content)
+  it 'shows the profile page' do
+    visit profile_path
+    expect(page).not_to have_content(@error_content)
+  end
 
-        visit admin_user_path(User.last)
-        expect(page).to have_content(@error_content)
+  it 'gives 404 at dashboard' do
+    visit dashboard_path
+    expect(page).to have_content(@error_content)
+  end
 
-        visit admin_merchant_path(User.last)
-        expect(page).to have_content(@error_content)
-      end
+  it 'shows the cart' do
+    visit cart_path
+    expect(page).not_to have_content(@error_content)
+  end
+
+  context 'visit any admin path' do
+    it 'gives 404' do
+
+      visit admin_dashboard_path
+      expect(page).to have_content(@error_content)
+
+      visit admin_users_path
+      expect(page).to have_content(@error_content)
+
+      visit admin_user_path(User.last)
+      expect(page).to have_content(@error_content)
+
+      visit admin_merchant_path(User.last)
+      expect(page).to have_content(@error_content)
     end
+  end
+end
 
-    context 'as a merchant' do
-      it 'gives 404' do
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
-        visit admin_dashboard_path
-        expect(page).to have_content(@error_content)
+RSpec.describe 'as a merchant' do
+  before :each do
+    @error_content = "The page you were looking for doesn't exist"
+    @merchant = create(:merchant)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
+  end
 
-        visit admin_users_path
-        expect(page).to have_content(@error_content)
+  it 'gives 404 at profile page' do
+    visit profile_path
+    expect(page).to have_content(@error_content)
+  end
 
-        visit admin_user_path(User.last)
-        expect(page).to have_content(@error_content)
+  it 'shows the dashboard' do
+    visit dashboard_path
+    expect(page).not_to have_content(@error_content)
+  end
 
-        visit admin_merchant_path(User.last)
-        expect(page).to have_content(@error_content)
-      end
+  it 'gives 404 at the cart' do
+    visit cart_path
+    expect(page).to have_content(@error_content)
+  end
+
+  context 'visit any admin path' do
+    it 'gives 404' do
+
+      visit admin_dashboard_path
+      expect(page).to have_content(@error_content)
+
+      visit admin_users_path
+      expect(page).to have_content(@error_content)
+
+      visit admin_user_path(User.last)
+      expect(page).to have_content(@error_content)
+
+      visit admin_merchant_path(User.last)
+      expect(page).to have_content(@error_content)
     end
+  end
+end
 
-    context 'as an admin' do
-      it 'shows page' do
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
-        visit admin_dashboard_path
-        expect(page).not_to have_content(@error_content)
+RSpec.describe 'as an admin' do
+  before :each do
+    @error_content = "The page you were looking for doesn't exist"
+    @admin = create(:admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+  end
 
-        visit admin_users_path
-        expect(page).not_to have_content(@error_content)
+  it 'gives 404 at profile page' do
+    visit profile_path
+    expect(page).to have_content(@error_content)
+  end
 
-        visit admin_user_path(User.last)
-        expect(page).not_to have_content(@error_content)
+  it 'gives 404 at dashboard' do
+    visit dashboard_path
+    expect(page).to have_content(@error_content)
+  end
 
-        visit admin_merchant_path(User.last)
-        expect(page).not_to have_content(@error_content)
-      end
+  it 'give 404 at cart' do
+    visit cart_path
+    expect(page).to have_content(@error_content)
+  end
+
+  context 'visit any admin path' do
+    it 'shows page' do
+      visit admin_dashboard_path
+      expect(page).not_to have_content(@error_content)
+
+      visit admin_users_path
+      expect(page).not_to have_content(@error_content)
+
+      visit admin_user_path(User.last)
+      expect(page).not_to have_content(@error_content)
+
+      visit admin_merchant_path(User.last)
+      expect(page).not_to have_content(@error_content)
     end
   end
 end
