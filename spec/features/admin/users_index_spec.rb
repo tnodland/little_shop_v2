7c28a8fbd718f581daed656a6172e49bff51aef0
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-04-03 15:05:22 -0600

    Adjusts testing params to make sure pathing was correct

--- spec/features/admin/users_index_spec.rb
+++ spec/features/admin/users_index_spec.rb
@@ -6,19 +6,24 @@ RSpec.describe "Admin User Index page", type: :feature do
       @admin = create(:admin)
       @user_1 = create(:user)
       @user_2 = create(:user)
+      @merchant_1 = create(:merchant)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
     end
 
     it 'shows information about each user' do
-      visit '/admin/users'
 
+      visit '/admin/users'
+      
       within "#user-#{@user_1.id}" do
-        expect(page).to have_link("User: #{@user_1.name}")
+        expect(page).to have_link("#{@user_1.name}")
         expect(page).to have_content("Date Registered: #{@user_1.created_at}")
         expect(page).to have_button("Upgrade to Merchant")
       end
 
       within "#user-#{@user_2.id}" do
-        expect(page).to have_link("User: #{@user_2.name} Date Registered: #{@user_2.created_at}")
+        expect(page).to have_link("#{@user_2.name}")
+        expect(page).to have_content("Date Registered: #{@user_2.created_at}")
         expect(page).to have_button("Upgrade to Merchant")
       end
     end
