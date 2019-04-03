04e70733c45062c6a9f75e016f0eaf88bf9b9bdb
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-04-03 15:05:22 -0600

    Adds tests for user order cancellation

--- spec/features/user/profile_order_show_spec.rb
+++ spec/features/user/profile_order_show_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe 'Profile Orders Show', type: :feature do
   describe 'As a Registerd User' do
     before :each do
       @user = create(:user)
-      @order_1 = create(:order, user_id: @user.id)
+      @order_1 = create(:order, user_id: @user.id, status: 0)
       @item_1 = create(:item)
       @item_2 = create(:item)
       @order_item_1 = create(:order_item, quantity: 5, ordered_price: 5.0, order: @order_1, item: @item_1)
-      create(:order_item, quantity: 4, ordered_price: 10.0, order: @order_1, item: @item_2)
+      @order_item_2 = create(:order_item, quantity: 4, ordered_price: 10.0, order: @order_1, item: @item_2)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     end
 
@@ -38,5 +38,47 @@ RSpec.describe 'Profile Orders Show', type: :feature do
         expect(page).to have_content("Subtotal: #{@order_item_1.subtotal}")
       end
     end
+
+    it 'lets a user cancel a pending order' do
+      item_1_quantity = @item_1.quantity
+      item_2_quantity = @item_2.quantity
+
+
+      visit profile_order_path(@order_1)
+
+      within '#order-info' do
+        expect(page).to have_button("Cancel Order")
+        click_button "Cancel Order"
+      end
+
+      expect(page).to have_content("cancelled")
+      expect(page).to_not have_content("pending")
+      expect(page).to_not have_content("packaged")
+      expect(page).to_not have_content("shipped")
+
+      expect(@order_item_1.fulfilled?).to be_false
+      expect(@order_item_2.fulfilled?).to be_false
+      expect(@item_1.quantity).to eq((item_1_quantity + @order_item_1.quantity))
+      expect(@item_2.quantity).to eq((item_2_quantity + @order_item_2.quantity))
+    end
+  end
+
+  it 'only shows the cancel order button if a package is pending' do
+    order_1 = create(:order, status: 1)
+    order_2 = create(:order, status: 2)
+    order_3 = create(:order, status: 3)
+    order_4 = create(:order, status: 0)
+
+    visit profile_order_path(order_4)
+    expect(page).to have_content("Cancel Order")
+
+    visit profile_order_path(order_1)
+    expect(page).to_not have_content("Cancel Order")
+
+    visit profile_order_path(order_2)
+    expect(page).to_not have_content("Cancel Order")
+
+    visit profile_order_path(order_3)
+    expect(page).to_not have_content("Cancel Order")
   end
 end
