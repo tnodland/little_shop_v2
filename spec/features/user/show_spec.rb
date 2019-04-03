db1ff24e2fa7a64bcf4e2bce43658e6a6656d3d5
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-04-03 15:05:22 -0600

    adds test for user profile path

---
+++ spec/features/user/show_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "User Index Page", type: :feature do
+  describe 'As a registered user' do
+    before :each do
+      @user = create(:user)
+    end
+    it 'shows information about the user and an edit button' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+      visit profile_path
+
+      expect(page).to have_content("Name: #{@user.name}")
+      expect(page).to have_content("Street Address: #{@user.street_adress}")
+      expect(page).to have_content("City: #{@user.city}")
+      expect(page).to have_content("State: #{@user.state}")
+      expect(page).to have_content("Zip Code: #{@user.zip_code}")
+      expect(page).to have_content("Email: #{@user.email}")
+      expect(page).to have_link("Edit Profile")
+      expect(page).to_not have_content(@user.password)
+    end
+  end
+end
