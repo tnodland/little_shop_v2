1213b6a8dba969e93a93837914b90c1f15137592
wipegup <wipegup@gmail.com>
2019-04-03 17:10:43 -0600

    Add test for updating cart as user

--- spec/features/cart/add_item_spec.rb
+++ spec/features/cart/add_item_spec.rb
@@ -3,31 +3,57 @@ require 'rails_helper'
 RSpec.describe 'Add Item to Cart' do
   before :each do
     @item = create(:item)
+    @user = create(:user)
   end
-  context 'From an item show page' do
-    it 'redirects to item index after clicking add to cart, displaying message that item has been added' do
-      visit item_path(@item)
-      click_button "Add to Cart"
-      expect(current_path).to eq(items_path)
-      expect(page).to have_content("You added #{@item.name} to your cart")
-    end
-  end
-  context 'Cart Total updates' do
-    it 'after adding first and second item' do
-      visit item_path(@item)
-      click_button "Add to Cart"
-
-      within "nav.main-nav" do
-        expect(page).to have_content("Cart: 1")
-      end
-
-      visit item_path(@item)
-      click_button "Add to Cart"
-
-      within "nav.main-nav" do
-        expect(page).to have_content("Cart: 2")
+  context 'as a visitor' do
+    context 'From an item show page' do
+      it 'redirects to item index after clicking add to cart, displaying message that item has been added' do
+        visit item_path(@item)
+        click_button "Add to Cart"
+        expect(current_path).to eq(items_path)
+        expect(page).to have_content("You added #{@item.name} to your cart")
       end
     end
+    context 'Cart Total updates' do
+      it 'after adding first and second item' do
+        visit item_path(@item)
+        click_button "Add to Cart"
+
+        within "nav.main-nav" do
+          expect(page).to have_content("Cart: 1")
+        end
+
+        visit item_path(@item)
+        click_button "Add to Cart"
+
+        within "nav.main-nav" do
+          expect(page).to have_content("Cart: 2")
+        end
+      end
+    end
+
   end
 
+  context 'As a user' do
+
+    context 'Cart Total updates' do
+      it 'after adding first and second item' do
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+        
+        visit item_path(@item)
+        click_button "Add to Cart"
+
+        within "nav.main-nav" do
+          expect(page).to have_content("Cart: 1")
+        end
+
+        visit item_path(@item)
+        click_button "Add to Cart"
+
+        within "nav.main-nav" do
+          expect(page).to have_content("Cart: 2")
+        end
+      end
+    end
+  end
 end
