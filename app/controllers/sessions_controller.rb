f7424e14f29be582d44b25b0794424b6e0f2ce55
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-03 22:26:57 -0600

    Add authentication logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    redirect_to profile_path
+    user = User.find_by(email: params[:email].downcase)
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id.to_s
+      redirect_to profile_path
+    else
+      flash.now.error = "Incorrect email address or password."
+      render :new
+    end
   end
 end
