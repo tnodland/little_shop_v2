99118594b8f5525a148746ecc286c1d614df89c3
Trevor Nodland <trevor.nodland@gmail.com>
2019-04-04 12:09:43 -0600

    finishes user story 42

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,7 @@ Rails.application.routes.draw do
     resources :users, only: [:index, :show]
     get '/merchants/:id', to: "merchants#show", as: :merchant
     get '/merchants', to: "merchants#index", as: :merchants
-    get '/merchant/:id/enable', to: "merchants#enable", as: :merchant_enable
-    get '/merchant/:id/disable', to: "merchants#disable", as: :merchant_disable
+    patch '/merchant/:merchant_id/', to: "merchants#update", as: :merchant_change_status
   end
 
   resources :users, only: [:create, :update]
