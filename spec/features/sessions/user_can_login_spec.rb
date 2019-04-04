7d0795fb5d5c0bba24379c02e872d9313d74be1f
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-03 22:39:46 -0600

    Add tests for logging in as a merchant or admin

--- spec/features/sessions/user_can_login_spec.rb
+++ spec/features/sessions/user_can_login_spec.rb
@@ -16,4 +16,36 @@ feature 'Logging In' do
       expect(page).to have_content("Welcome back, #{user.name}! You are now logged in.")
     end
   end
+
+  scenario 'as a Merchant' do
+    user = create(:merchant)
+    visit login_path
+
+    fill_in 'email', with: user.email
+    fill_in 'password', with: user.password
+
+    click_button 'Log In'
+
+    expect(page).to have_current_path(dashboard_path)
+    expect(page).to have_http_status(200)
+    within 'div.alert' do
+      expect(page).to have_content("You are now logged in.")
+    end
+  end
+
+  scenario 'as an Admin' do
+    user = create(:admin)
+    visit login_path
+
+    fill_in 'email', with: user.email
+    fill_in 'password', with: user.password
+
+    click_button 'Log In'
+
+    expect(page).to have_current_path(root_path)
+    expect(page).to have_http_status(200)
+    within 'div.alert' do
+      expect(page).to have_content("You are now logged in.")
+    end
+  end
 end
