e47172c62bd2f3a2c945037fe10eaf447216540a
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-03 23:05:25 -0600

    Remap logout path to destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
-  get '/logout', to: "sessions#show"
+  get '/logout', to: "sessions#destroy"
   get '/register', to: "users#new"
 
   resources :items, only: [:index, :show]
