68459ee78da61295de99a0b4aaf1cd56b4ae2aa6
Trevor Nodland <trevor.nodland@gmail.com>
2019-04-04 09:27:13 -0600

    finishes user story 16

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -5,7 +5,10 @@ RSpec.describe "Item show page" do
     it "should see information about an item" do
       merchant = create(:merchant)
       item = create(:item, user: merchant)
-
+      shopper = create(:user)
+      order = create(:shipped_order, user: shopper)
+      create(:fulfilled_order_item, order: order, item: item, quantity: 10, created_at: "Wed, 03 Apr 2019 14:10:25 UTC +00:00", updated_at: "Thu, 04 Apr 2019 14:11:25 UTC +00:00")
+      create(:fulfilled_order_item, order: order, item: item, quantity: 5, created_at: "Wed, 03 Apr 2019 14:11:25 UTC +00:00", updated_at: "Thu, 04 Apr 2019 14:11:25 UTC +00:00")
       visit item_path(item)
 
       expect(page).to have_content(item.name)
@@ -14,7 +17,7 @@ RSpec.describe "Item show page" do
       expect(page).to have_xpath("//img[@src='https://vignette.wikia.nocookie.net/animalcrossing/images/7/72/Tom_Nook.png/revision/latest?cb=20101105231130']")
       expect(page).to have_content("Number in stock: #{item.quantity}")
       expect(page).to have_content("Price: #{item.current_price}")
-      expect(page).to have_content("Average Processing Time:")
+      expect(page).to have_content("Average Processing Time: 1.0 Day(s)")
       expect(page).to have_link("Add to Cart")
     end
   end
