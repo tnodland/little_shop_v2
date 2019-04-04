f8fff75c3d9a6d58d838011628ac837c6f28d872
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-03 22:40:32 -0600

    Add logic to redirect after successful login based on user type

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,29 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email].downcase)
-    if user&.authenticate(params[:password])
-      session[:user_id] = user.id.to_s
-      flash.notice = "Welcome back, #{user.name}! You are now logged in."
-      redirect_to profile_path
+    @user = User.find_by(email: params[:email].downcase)
+    if @user&.authenticate(params[:password])
+      session[:user_id] = @user.id.to_s
+      login_redirect
     else
       flash.now.error = "Incorrect email address or password."
       render :new
+    end
+  end
+
+  private
+
+  def login_redirect
+    case viewer_category
+    when 'user'
+      flash.notice = "Welcome back, #{@user.name}! You are now logged in."
+      redirect_to profile_path
+    when 'merchant'
+      flash.notice = "You are now logged in."
+      redirect_to dashboard_path
+    when 'admin'
+      flash.notice = "You are now logged in."
+      redirect_to root_path
     end
   end
 end
