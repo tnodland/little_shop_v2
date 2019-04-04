ef66ec23d8a95233fd49d20fd7d1c4f53c43cb48
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-03 22:47:54 -0600

    Add tests for invalid credentials

---
+++ spec/features/sessions/login_authentication_spec.rb
@@ -0,0 +1,78 @@
+require 'rails_helper'
+
+feature 'Logging In' do
+  describe 'with valid credentials' do
+    scenario 'as a User' do
+      user = create(:user)
+      visit login_path
+
+      fill_in 'email', with: user.email
+      fill_in 'password', with: user.password
+
+      click_button 'Log In'
+
+      expect(page).to have_current_path(profile_path)
+      expect(page).to have_http_status(200)
+      within 'div.alert' do
+        expect(page).to have_content("Welcome back, #{user.name}! You are now logged in.")
+      end
+    end
+
+    scenario 'as a Merchant' do
+      user = create(:merchant)
+      visit login_path
+
+      fill_in 'email', with: user.email
+      fill_in 'password', with: user.password
+
+      click_button 'Log In'
+
+      expect(page).to have_current_path(dashboard_path)
+      expect(page).to have_http_status(200)
+      within 'div.alert' do
+        expect(page).to have_content("You are now logged in.")
+      end
+    end
+
+    scenario 'as an Admin' do
+      user = create(:admin)
+      visit login_path
+
+      fill_in 'email', with: user.email
+      fill_in 'password', with: user.password
+
+      click_button 'Log In'
+
+      expect(page).to have_current_path(root_path)
+      expect(page).to have_http_status(200)
+      within 'div.alert' do
+        expect(page).to have_content("You are now logged in.")
+      end
+    end
+  end
+
+  scenario 'with invalid credentials' do
+    user = create(:user)
+    visit login_path
+
+    fill_in 'email', with: "nonsense"
+    fill_in 'password', with: user.password
+
+    click_button 'Log In'
+
+    expect(page).to have_current_path(login_path)
+    within 'div.alert' do
+      expect(page).to have_content("Incorrect email address or password.")
+    end
+
+    fill_in 'email', with: user.email
+    fill_in 'password', with: "more nonsense"
+
+    click_button 'Log In'
+
+    expect(page).to have_current_path(login_path)
+    within 'div.alert' do
+      expect(page).to have_content("Incorrect email address or password.")
+    end
+  end
+end
