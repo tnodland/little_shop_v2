c65f1baf65ae1858dc8cc8a14ebb0094495b2f60
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-03 22:32:30 -0600

    Set flash notice upon successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email].downcase)
     if user&.authenticate(params[:password])
       session[:user_id] = user.id.to_s
+      flash.notice = "Welcome back, #{user.name}! You are now logged in."
       redirect_to profile_path
     else
       flash.now.error = "Incorrect email address or password."
