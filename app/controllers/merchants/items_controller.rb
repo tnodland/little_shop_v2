67f2566a51f31b4fa52a92d5c759b0f78d760615
wipegup <wipegup@gmail.com>
2019-04-06 17:32:36 -0600

    Passing flash message test for dis/enabled

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -6,7 +6,8 @@ class Merchants::ItemsController < Merchants::BaseController
   def update
     @item = Item.find(params[:id])
     @item.update(enabled: params[:enabled])
-
+    completed_action = params[:enabled] == "true" ? "Enabled": "Disabled"
+    flash[:info] = [@item.name, completed_action].join(" ")
     redirect_to dashboard_items_path
   end
 end
