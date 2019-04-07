1e20d45f67318181724def1cac90d02a79a7ed72
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-06 22:02:50 -0600

    Implement Order.from_cart() creation method

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,16 @@ class Order < ApplicationRecord
   has_many :items, through: :order_items
 
   enum status: ['pending', 'packaged', 'shipped', 'cancelled']
+
+  def self.from_cart(user, cart)
+    fresh_order = self.create(user: user, status: 'pending')
+    cart.each do |item_id, quantity|
+      item = Item.find(item_id)
+      fresh_order.order_items.create(item_id: item_id,
+                                    quantity: quantity,
+                               ordered_price: item.current_price,
+                                  created_at: fresh_order.created_at)
+    end
+    return fresh_order
+  end
 end
