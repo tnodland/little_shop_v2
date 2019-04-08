ede39a885b1c7f2acec97645872c6cdb52525dde
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-04-08 11:05:02 -0600

    adds link and route as well as action that disables all items associated with that merchant as well as downgrading the merchant

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -13,4 +13,19 @@ class Admin::MerchantsController < Admin::BaseController
     merchant.save
     redirect_to admin_merchants_path
   end
+
+  def downgrade
+    merchant = User.find(params[:id])
+    # binding.pry
+    if merchant.merchant?
+      merchant.items.each do |item|
+        item.enabled = false
+        item.save
+      end
+      merchant.role = 0
+      merchant.save
+      flash[:notice] = "#{merchant.name} is now a User"
+      redirect_to admin_user_path(merchant)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     get '/dashboard', to: "orders#index"
     resources :users, only: [:index, :show]
     get '/admin/users/:id/upgrade', to: "users#upgrade", as: 'upgrade_user'
+    get '/admin/merchants/:id/downgrade', to: "merchants#downgrade", as: 'downgrade_merchant'
     get '/merchants/:id', to: "merchants#show", as: :merchant
     get '/merchants', to: "merchants#index", as: :merchants
     patch '/merchant/:merchant_id/', to: "merchants#update", as: :merchant_change_status
