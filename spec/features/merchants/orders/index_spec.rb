ebc4e2b8fcddb9cbc790345feed410d95abdf9d0
wipegup <wipegup@gmail.com>
2019-04-08 12:44:53 -0600

    Add test for showing all pending orders

--- spec/features/merchants/orders/index_spec.rb
+++ spec/features/merchants/orders/index_spec.rb
@@ -3,11 +3,16 @@ require 'rails_helper'
 RSpec.describe 'Merchant Item Index', type: :feature do
   before :each do
     @merchant = create(:merchant)
-    # @items = create_list(:item,2,  user: @merchant)
-    # @inactive_item = create(:inactive_item, user: @merchant)
-    #
-    # @order_items_1 = create_list(:order_item, 3, item:@items[0])
-    # @order_item_inactive = create(:order_item, item:@inactive_item)
+    @users = create_list(:user, 2))
+    @items = create_list(:item, 2,  user: @merchant)
+
+    @order_1 = create(:order, user: @users[0])
+    @order_2 = create(:order, user: @users[1])
+    @shipped_order = create(:shipped_order, user: @users[1])
+
+    @order_items_1 = create(:order_item, item:@items[0], order:@order_1)
+    @order_items_2 = create(:order_item, item:@items[0], order:@order_2)
+    @order_items_3 = create(:order_item, item:@items[1], order:@order_2)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
   end
@@ -20,6 +25,26 @@ RSpec.describe 'Merchant Item Index', type: :feature do
     expect(page).to have_content(@merchant.state)
     expect(page).to have_content(@merchant.zip_code)
     expect(page).to have_content(@merchant.email)
+  end
+
+  it 'Shows all pending orders' do
+    visit dashboard_path
+    within "#order-#{@order_1.id}" do
+      expect(page).to have_link(@order_1.id, dashboard_order_path(@order_1))
+      expect(page).to have_content(@order_1.created_at)
+      expect(page).to have_content(@order_1.total_count)
+      expect(page).to have_content(@order_1.total_cost)
+    end
+
+    within "#order-#{@order_2.id}" do
+      expect(page).to have_link(@order_2.id, dashboard_order_path(@order_2))
+      expect(page).to have_content(@order_2.created_at)
+      expect(page).to have_content(@order_2.total_count)
+      expect(page).to have_content(@order_2.total_cost)
+    end
+
+    expect(page).not_to have_selector('div', id:"#order=#{@order_3.id}")
+
   end
 
 end
