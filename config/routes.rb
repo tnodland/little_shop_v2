da01353dc34906e8d692ff147ce52162db0e1040
wipegup <wipegup@gmail.com>
2019-04-08 13:22:39 -0600

    Organize Routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,37 +11,34 @@ Rails.application.routes.draw do
     patch '/merchant/:merchant_id/', to: "merchants#update", as: :merchant_change_status
   end
 
+  resources :items, only: [:index, :show]
+  resources :orders, only: [:create]
   resources :users, only: [:create, :update]
 
   namespace :profile do
     resources :orders, only: [:index, :show]
   end
+
+  get '/profile/orders', to: "orders#index"
   post 'profile/orders/:id', to: 'profile/orders#cancel', as: 'cancel_order'
   get '/profile', to: "users#show", as: 'profile'
   get '/profile/edit', to: "users#edit", as: 'edit_profile'
-  # get '/dashboard/items', to: "merchants/items#index"
+
+  get '/dashboard', to: 'merchants/orders#index'
   scope :dashboard, module: :merchants, as: :dashboard do
     resources :items, only: [:index, :destroy, :update, :new, :create, :edit]
-
+    resources :orders, only: [:show]
   end
 
-  get '/dashboard/items', to: "merchants/items#index"
-  get '/dashboard', to: 'merchants/orders#index'
-
-  get '/profile', to: "users#show"
-  get '/profile/orders', to: "orders#index"
   get '/cart', to: 'carts#show'
   delete '/cart', to: 'carts#destroy'
   patch '/cart', to: 'carts#update'
   resources :carts, only: [:create]
 
   get '/merchants', to: "merchants#index"
-
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   get '/logout', to: "sessions#destroy"
   get '/register', to: "users#new"
 
-  resources :items, only: [:index, :show]
-  resources :orders, only: [:create]
 end
