8fc0468f6c90dc0ecc4a4aa73663ba2e5375bc52
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-04-07 23:12:37 -0600

    adds tests for admin user show page

--- spec/features/admin/users_index_spec.rb
+++ spec/features/admin/users_index_spec.rb
@@ -8,13 +8,12 @@ RSpec.describe "Admin User Index page", type: :feature do
       @user_2 = create(:user)
       @merchant_1 = create(:merchant)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
-
     end
 
     it 'shows information about each user' do
 
       visit '/admin/users'
-      
+
       within "#user-#{@user_1.id}" do
         expect(page).to have_link("#{@user_1.name}")
         expect(page).to have_content("Date Registered: #{@user_1.created_at}")
