c2ca7b12d6322e3bba08ce78fa7c252d4a87250a
wipegup <wipegup@gmail.com>
2019-04-08 13:48:23 -0600

    recreate fulfilled order_item

--- spec/factories/order_items.rb
+++ spec/factories/order_items.rb
@@ -7,6 +7,9 @@ FactoryBot.define do
     fulfilled { false }
   end
 
+  factory :fulfilled_order_item, parent: :order_item do
+    fulfilled { true }
+  end
   factory :fast_fulfilled_order_item, parent: :order_item do
     created_at { "Wed, 03 Apr 2019 14:10:25 UTC +00:00" }
     updated_at { "Thu, 04 Apr 2019 14:11:25 UTC +00:00" }
