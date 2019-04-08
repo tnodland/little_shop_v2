7e3f0eb1b6e4825de8e6f2826759995f06254aee
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-08 10:46:20 -0600

    Update tests to unify terminology

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe "Items index page" do
         expect(page).to have_link(@item1.name)
         expect(page).to have_xpath("//img[@src='https://vignette.wikia.nocookie.net/animalcrossing/images/7/72/Tom_Nook.png/revision/latest?cb=20101105231130']")
         expect(page).to have_content("Sold by: #{@merchant1.name}")
-        expect(page).to have_content("Quantity: #{@item1.quantity}")
+        expect(page).to have_content("Number in stock: #{@item1.quantity}")
         expect(page).to have_content("Price: $#{@item1.current_price}")
       end
 
