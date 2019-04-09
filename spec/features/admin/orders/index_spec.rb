f210bb8d780e311c229faf07ee160baf01c304e1
wipegup <wipegup@gmail.com>
2019-04-09 07:03:41 -0600

    Test for content on admin dash

--- spec/features/admin/orders/index_spec.rb
+++ spec/features/admin/orders/index_spec.rb
@@ -10,10 +10,24 @@ RSpec.describe 'As an Admin User' do
       @pending_orders = 4.times.map{ create(:packaged_order, user:@users[rand(4)])}
       @shipped_orders = 4.times.map{ create(:packaged_order, user:@users[rand(4)])}
       @cancelled_orders = 4.times.map{ create(:packaged_order, user:@users[rand(4)])}
-      
+
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
     end
 
+    it 'shows all order information' do
+      all_orders = @packaged_orders + @pending_orders + @shipped_orders + @cancelled_orders
+
+      visit admin_dashboard_path
+
+      all_orders.each do |order|
+        within "#order-#{order.id}" do
+          expect(page).to have_content(order.created_at)
+          expect(page).to have_content(order.status)
+          expect(page).to have_content("Order: #{order.id}")
+          expect(page).to have_link(order.user.id, href:admin_user_path(order.user))
+        end
+      end
+    end
 
   end
 end
