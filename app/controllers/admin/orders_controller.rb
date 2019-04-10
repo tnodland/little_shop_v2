de0d6abfdf5ee0c4906d95f520dec02f2bb6b667
wipegup <wipegup@gmail.com>
2019-04-10 13:16:11 -0600

    Drying up controller code

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,28 +1,12 @@
 class Admin::OrdersController < Admin::BaseController
+  include Profile::OrdersController::UserOrder
+
   def index
     @orders = Order.admin_ordered
   end
-
-  def show
-    @order = Order.find(params[:id])
-  end
-
+  
   def cancel
-    order = Order.find(params[:id])
-    if order.pending?
-      order.order_items.each do |order_item|
-        if order_item.fulfilled?
-          order_item.fulfilled = false
-          order_item.item.quantity += order_item.quantity
-          order_item.item.save
-          order_item.save
-        end
-      end
-      order.status = 3
-      order.save
-      flash[:notice] = "The order has been cancelled"
-      redirect_to admin_dashboard_path
-    end
+    super(admin_dashboard_path)
   end
 
   def update
--- app/controllers/profile/orders_controller.rb
+++ app/controllers/profile/orders_controller.rb
@@ -4,26 +4,28 @@ class Profile::OrdersController < Profile::BaseController
     @orders = Order.where(user: current_user)
   end
 
-  def show
-    @order = Order.find(params[:id])
-    @line_items = @order.order_items
-  end
+  module UserOrder
+    def show
+      @order = Order.find(params[:id])
+    end
 
-  def cancel
-    order = Order.find(params[:id])
-    if order.pending?
-      order.order_items.each do |order_item|
-        if order_item.fulfilled?
-          order_item.fulfilled = false
-          order_item.item.quantity += order_item.quantity
-          order_item.item.save
-          order_item.save
+    def cancel(redirect_path = profile_path)
+      order = Order.find(params[:id])
+      if order.pending?
+        order.order_items.each do |order_item|
+          if order_item.fulfilled?
+            order_item.fulfilled = false
+            order_item.item.quantity += order_item.quantity
+            order_item.item.save
+            order_item.save
+          end
         end
+        order.status = 3
+        order.save
+        flash[:notice] = "The order has been cancelled"
+        redirect_to redirect_path
       end
-      order.status = 3
-      order.save
-      flash[:notice] = "Your order has been cancelled"
-      redirect_to profile_path
     end
   end
+
 end
