6d9bd24337cb9dc69bad53568db218792b418a52
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-10 17:05:54 -0600

    Fix regressions in admin orders

--- spec/features/admin/orders/show_spec.rb
+++ spec/features/admin/orders/show_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe 'As an Admin User' do
       visit admin_dashboard_path
 
       within "#order-#{@order_1.id}" do
-        click_link "Order: #{@order_1.id}"
+        click_link "Order ID: #{@order_1.id}"
       end
 
       expect(current_path).to eq(admin_user_order_path(@order_1.user, @order_1))
@@ -32,14 +32,14 @@ RSpec.describe 'As an Admin User' do
         expect(page).to have_content("Order Placed: #{@order_1.created_at.strftime("%m/%d/%Y")}")
         expect(page).to have_content("Last Updated: #{@order_1.updated_at.strftime("%m/%d/%Y")}")
         expect(page).to have_content("QTY of Items: #{@order_1.total_count}")
-        expect(page).to have_content("Grand Total: #{@order_1.total_cost}")
+        expect(page).to have_content("Grand Total: #{number_to_currency(@order_1.total_cost)}")
       end
 
       within "#item-#{@item_1.id}" do
         expect(page).to have_xpath("//img[@src='https://vignette.wikia.nocookie.net/animalcrossing/images/7/72/Tom_Nook.png/revision/latest?cb=20101105231130']")
         expect(page).to have_content("Item: #{@item_1.name}")
         expect(page).to have_content("QTY: #{@order_item_1.quantity}")
-        expect(page).to have_content("Subtotal: #{@order_item_1.subtotal}")
+        expect(page).to have_content("Subtotal: #{number_to_currency(@order_item_1.subtotal)}")
       end
     end
 
