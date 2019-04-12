c9ee9b7a4b987103026cac90f5bc0d6b68e93048
wipegup <wipegup@gmail.com>
2019-04-12 07:36:33 -0600

    Testing pages have content

--- spec/features/merchants/orders/csv_spec.rb
+++ spec/features/merchants/orders/csv_spec.rb
@@ -61,10 +61,12 @@ RSpec.describe 'Merchant Orders Index (Dashboard)', type: :feature do
 
     expect(page).to have_link("Current Customer Data", href:dashboard_current_csv_path(format: :csv))
     click_link "Current Customer Data"
+    expect(page).to have_content("Name")
 
     visit dashboard_path
     expect(page).to have_link("Potential Customer Data", href:dashboard_potential_csv_path(format: :csv))
     click_link "Potential Customer Data"
+    expect(page).to have_content("Name")
   end
 
 end
