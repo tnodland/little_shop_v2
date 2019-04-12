8bc8fbaf0eb13f21e568be5e609656fe89c686e0
wipegup <wipegup@gmail.com>
2019-04-12 07:51:54 -0600

    Some styling to separate out links for csvs

--- spec/features/merchants/orders/csv_spec.rb
+++ spec/features/merchants/orders/csv_spec.rb
@@ -58,7 +58,6 @@ RSpec.describe 'Merchant Orders Index (Dashboard)', type: :feature do
 
   it 'has a link to two csv pages, one for current users, and one for non-users that are on the site' do
     visit dashboard_path
-
     expect(page).to have_link("Current Customer Data", href:dashboard_current_csv_path(format: :csv))
     click_link "Current Customer Data"
     expect(page).to have_content("Name,Email,Merchant Revenue,Total Revenue")
